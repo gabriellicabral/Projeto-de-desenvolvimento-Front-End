c4c7a6dbf17d4fd49d5841bee1130b61dd94da36
Gabrielli Cabral <gabrielli.cabral2017@gmail.com>
2026-03-08 20:10:53 -0300

    Adicionando contéudo a seção Planos

--- landingpage/src/pages/Planos.jsx
+++ landingpage/src/pages/Planos.jsx
@@ -15,16 +15,29 @@ function Planos(){
       <div className="cardplanos">
         <h3>Básico</h3>
         <p>Pagamento por corrida</p>
+        <h4>Motoristas verificados</h4>
+        <h4>Corrida sob demanda</h4>
+        <h4>Suporte via Chat</h4>
       </div>
 
       <div className="cardplanos"> 
         <h3>Escolar</h3>
         <p>Para uso diário escolar</p>
+        <h4>Motoristas verificados</h4>
+        <h4>Prioridade de corrida</h4>
+        <h4>Suporte 24/7 em todos os nossos meios</h4>
+        <h4>Desconto em Serviços</h4>
       </div>
 
       <div className="cardplanos">
         <h3>Premium</h3>
         <p>Atendimento prioritário</p>
+        <h4>Motoristas verificados</h4>
+        <h4>Prioridade de corrida</h4>
+        <h4>Suporte 24/7 em todos os nossos meios</h4>
+        <h4>Desconto em Serviços</h4>
+        <h4>Acesso de emergência</h4>
+        <h4>Corridas Ilimitadas</h4>
       </div>
       </div>
     </section>
