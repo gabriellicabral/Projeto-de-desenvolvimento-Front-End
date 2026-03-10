02b4e1da9df35699bf264551d9f8c4c13e8d0ec2
Gabrielli Cabral <gabrielli.cabral2017@gmail.com>
2026-03-10 14:12:57 -0300

    adicionando forms

--- landingpage/src/pages/Cadastro.jsx
+++ landingpage/src/pages/Cadastro.jsx
@@ -13,8 +13,57 @@ function Cadastro(){
         <section className="secaodecadastro">
 
         {/* Área destinada ao cadastro de usuários */}
-        <div>
-            <h1>Cadastro de usuário</h1>
+        <div className="cadastrodiv">
+               <form>
+
+                    <h2>Cadastro do Responsável</h2>
+
+                    <label>Nome completo</label>
+                    <input type="text" name="nome" required />
+
+                    <label>CPF</label>
+                    <input type="text" name="cpf" />
+
+                    <label>Telefone</label>
+                    <input type="tel" name="telefone" />
+
+                    <label>Endereço</label>
+                    <input type="text" name="endereco" />
+
+                    <label>Forma de pagamento</label>
+                    <select name="pagamento">
+                        <option>Pix</option>
+                        <option>Cartão</option>
+                        <option>Dinheiro</option>
+                    </select>
+
+                    <h2>Informações do Passageiro</h2>
+
+                    <label>Tipo de deficiência</label>
+                    <input type="text" name="deficiencia" />
+
+                    <label>Necessidades específicas</label>
+                    <textarea name="necessidades"></textarea>
+
+                    <label>Usa cadeira de rodas?</label>
+                    <select name="cadeira">
+                        <option>Sim</option>
+                        <option>Não</option>
+                    </select>
+
+                    <label>Necessita acompanhante?</label>
+                    <select name="acompanhante">
+                        <option>Sim</option>
+                        <option>Não</option>
+                    </select>
+
+                    <button type="submit">Cadastrar</button>
+
+                </form>
+            
+
+
+            
         </div>
 
         </section>
